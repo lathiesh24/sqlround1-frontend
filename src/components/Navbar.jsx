81b26f6ff79cee2b815ef22cee1bcfb5a7c56079
lathiesh24 <lathumahi24@gmail.com>
2023-10-31 10:31:42 +0530

    rece

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white flex  p-6">
+      <div className="text-black text-xl font-medium">Query Quest</div>
+    </nav>
+  );
+};
+
+export default Navbar;
